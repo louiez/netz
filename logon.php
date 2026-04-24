6a3fdd4afcba31fe4ed1ee6de4eb0612a94f1e18
louiez <louiez@users.noreply.github.com>
2026-04-24 17:29:44 -0500

    logon.php fix

--- logon.php
+++ logon.php
@@ -2,7 +2,7 @@
 // Secure session settings (must be before session_start())
 ini_set('session.use_only_cookies', 1);
 ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
+ini_set('session.cookie_secure', 0);
 
 session_start();
 ob_start();
